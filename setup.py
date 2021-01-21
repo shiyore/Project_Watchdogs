5b1875cc5089944e34c2fa234cb2e02f271ad9e9
root <root@localhost.localdomain>
2021-01-21 14:14:59 -0800

    Added headless setup to install script

--- setup.py
+++ setup.py
@@ -23,13 +23,78 @@ def check_sudo():
 #install methods
 def packages():
     print(Fore.GREEN + "Installing packages...")
+    install_string = "openssh-server apache2 libapache2-mod-wsgi-py3 python-dev"
+    os.system(" apt-get install " + install_string)    
 
 def files():
     print(Fore.GREEN + "Setting up files...")
 
-#--------------------------------------main-------------------------------------------
+#--------------------------------------Modules----------------------------------------
 #print("args length: " + str(len(sys.argv)))
+def headless_setup():
+    #setting up ssh
+    os.system("update-rc.d -f ssh remove")
+    os.system("update-rc.d -f ssh defaults")
 
+    #backing up ssh keys and generating new ones
+    os.system("cd /etc/ssh/ & mkdir insecure_old")
+    os.system("mv ssh_host* insecure_old")
+    os.system("dpkg-reconfigure openssh-server")
+
+    #enabling login without password
+    read_file = open("/etc/ssh/sshd_config", "r")
+    lines = read_file.readlines()
+    i = 0
+    for line in lines:
+        if("PermitRootLogin" in line):
+            lines[i] = "PermitRootLogin yes\n"
+        i +=1
+    
+    read_file.close()
+    write_file = open("/etc/ssh/sshd_config", "w")
+    write_file.writelines(lines)
+    write_file.close()
+    
+    #restarting ssh
+    os.system("service ssh restart")
+    os.system("update-rc.d -f ssh enable 2 3 4 5")
+
+    #setting up autologin 
+    read_file = open("/etc/lightdm/lightdm.conf", "r")
+    lines = read_file.readlines()
+    i = 0
+    for line in lines:
+        if("autologin-user " in line):
+            lines[i] = line[1:]
+        i +=1
+    i = 0
+    for line in lines:
+        if("autologin-user-timeout" in line):
+            lines[i] = line[1:]
+        i +=1   
+ 
+    read_file.close()
+    write_file = open("/etc/lightdm/lightdm.conf", "w")
+    write_file.writelines(lines)
+    write_file.close()
+    
+    read_file = open("/etc/pam.d/lightdm-autologin", "r")
+    lines = read_file.readlines()
+    i = 0
+    for line in lines:
+        if("auth required pam_succeed_if.so user != root quiet_success" in line):
+            lines[i] = "###" + line
+        i +=1
+    read_file.close()
+    write_file = open("/etc/pam.d/lightdm-autologin", "w")
+    write_file.writelines(lines)
+    write_file.close()
+
+def apache_setup():
+    os.system("service apache2 enable & service apache2 restart")
+    os.system("a2enmod wsgi")
+     
+#--------------------------------------main-------------------------------------------
 check_sudo()
 
 if(len(sys.argv) > 1):
@@ -37,6 +102,8 @@ if(len(sys.argv) > 1):
         #print("Installing")
         packages()        
         files()
+        apache_setup()
+        headless_setup()        
 
     elif(str(sys.argv[1]) == "-h"):
         print("To install the project run setup.py install")
